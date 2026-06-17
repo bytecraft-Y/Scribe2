a268f35469a46795a5c2e127d8987e5b0d221d2e
bytecraft-Y <yyvvyashvats@gmail.com>
2026-06-17 13:10:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def to_vtt_time(seconds):
 # Load AI Model Natively
 @st.cache_resource
 def load_model():
-    return WhisperModel("small", device="cpu", compute_type="int8")
+    return WhisperModel("large-v3", device="cuda", compute_type="float16")
 
 model = load_model()
 
